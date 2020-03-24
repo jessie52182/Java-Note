57cbe7001e79ca9a4af8dc375c6fd0f48a83f72b
jessie52182 <jessie52182@gmail.com>
2020-03-23 22:52:44 -0400

    Add files via upload

---
+++ A example of ArrayList.java
@@ -0,0 +1,31 @@
+/* A example of an ArrayList */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Java_0122 {
+    
+    public static void main(String[] args) {
+         
+        ArrayList <String> studentNameList = new ArrayList<String> ();
+        String studentName;
+        
+        System.out.println("Please enter four names of student.");
+        
+        for (int a = 0; a < 4; a++) 
+        {
+            System.out.print("Number" + (a+1) + ": ");
+            Scanner input = new Scanner(System.in); 
+            studentName = input.nextLine(); 
+            studentNameList.add(studentName);
+        }
+        
+        System.out.println("The four names of students are: ");
+        
+        for(String name: studentNameList)           
+            System.out.println(name);  
+
+        System.exit(0);
+       
+    } 
+}
